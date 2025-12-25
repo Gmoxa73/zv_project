59aa2f63e825fb2fc91756dd48af2257ff75fd60
Dmitry <gmoxa@rambler.ru>
2025-12-25 13:58:46 +0300

    change models.py

--- bd/models.py
+++ bd/models.py
@@ -16,6 +16,7 @@ class Address(models.Model):
     lat = models.IntegerField(blank=True, null=True)
     lng = models.IntegerField(blank=True, null=True)
     adr_id = models.IntegerField(unique=True, blank=True, null=True)
+    cms = models.IntegerField(blank=True, null=True, unique=True)
     okrug = models.ForeignKey(Okrug, on_delete=models.CASCADE, related_name='back_addresses')
 
     def __str__(self):
@@ -25,7 +26,6 @@ class Device(models.Model):
     ip = models.CharField(max_length=15, unique=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='back_device')
     type = models.ForeignKey(TypeA, on_delete=models.CASCADE, related_name='back_type')
-    cms = models.IntegerField(blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.ip
